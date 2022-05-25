abf30eba7128d44283c0775305314f6464b06d60
Valentin Fuhlenbrok <vfuhlenb@c2s19d125.42wolfsburg.de>
2022-05-25 16:30:44 +0200

    testing

--- philo/main.c
+++ philo/main.c
@@ -6,13 +6,41 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 17:07:13 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2022/05/23 19:50:02 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2022/05/25 16:29:43 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	*myTurn(void *arg)
+{
+	while(1)
+	{
+		sleep(1);
+		printf("My Turn!\n");
+	}
+	return (NULL);
+}
+
+void	*yourTurn(void *arg)
+{
+	while(1)
+	{
+		sleep(3);
+		printf("Your Turn!\n");
+	}
+	return (NULL);
+}
+
 int	main(int argc, char *argv[])
 {
-//	pthread_create
+	pthread_t	thread1;
+	pthread_t	thread2;
+
+	while(1)
+	{
+	pthread_create(&thread1, NULL, myTurn, NULL);
+	pthread_create(&thread1, NULL, yourTurn, NULL);
+	}
+	return (0);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 17:13:15 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2022/05/23 18:23:17 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2022/05/25 16:23:18 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,11 @@
 # include <unistd.h>
 # include <sys/time.h>
 # include <pthread.h>
+# include <stdio.h>
+
+typedef struct s_data
+{
+	int	death;
+} t_data;
 
 #endif
