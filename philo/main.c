0e0fd9075d1afc704a8ba5d74b9e6db5f7184be5
Valentin Fuhlenbrok <vfuhlenb@c2s19d125.42wolfsburg.de>
2022-05-25 17:39:10 +0200

    wip

--- philo/main.c
+++ philo/main.c
@@ -6,27 +6,30 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 17:07:13 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2022/05/25 16:29:43 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2022/05/25 17:38:07 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	*myTurn(void *arg)
+void	*myTurn(t_data *data)
 {
 	while(1)
 	{
+		data->death = 2;
 		sleep(1);
 		printf("My Turn!\n");
+		
 	}
 	return (NULL);
 }
 
-void	*yourTurn(void *arg)
+void	*yourTurn(t_data *data)
 {
 	while(1)
 	{
-		sleep(3);
+		data->death = 1;
+		sleep(1);
 		printf("Your Turn!\n");
 	}
 	return (NULL);
@@ -36,11 +39,11 @@ int	main(int argc, char *argv[])
 {
 	pthread_t	thread1;
 	pthread_t	thread2;
+	t_data		data;
 
-	while(1)
-	{
-	pthread_create(&thread1, NULL, myTurn, NULL);
-	pthread_create(&thread1, NULL, yourTurn, NULL);
-	}
+	data.death = 0;
+	pthread_create(&thread1, NULL, (void*)myTurn, (void *)&data);
+	pthread_create(&thread2, NULL, (void*)yourTurn, (void *)&data);
+	pthread_join(thread2, NULL);
 	return (0);
 }
