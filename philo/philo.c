87ffc3e2aa80e2acc74c0c8220bc357c9d4f73ea
Valentin Fuhlenbrok <vfuhlenb@c2s19d125.42wolfsburg.de>
2022-06-15 21:27:25 +0200

    philo_eating

--- philo/philo.c
+++ philo/philo.c
@@ -6,23 +6,26 @@
 /*   By: vfuhlenb <vfuhlenb@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 11:40:18 by vfuhlenb          #+#    #+#             */
-/*   Updated: 2022/06/15 21:09:15 by vfuhlenb         ###   ########.fr       */
+/*   Updated: 2022/06/15 21:26:49 by vfuhlenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+static void	print_status(t_philo *philo, long long time, char *str)
+{
+	pthread_mutex_lock(&philo->data->status);
+	printf("%lldms	%d %s\n", time, philo->id, str);
+	pthread_mutex_unlock(&philo->data->status);
+}
+
 static int	philo_eating(t_philo *philo)
 {
 	pthread_mutex_lock(&philo->fork[philo->id - 1]);
-	pthread_mutex_lock(&philo->data->status);
-	printf("%lldms    %d has taken a fork\n", \
-		time_passed(philo->data->start), philo->id);
-	pthread_mutex_unlock(&philo->data->status);
-	pthread_mutex_lock(&philo->data->status);
-	printf("%lldms    %d is eating\n", \
-		time_passed(philo->data->start), philo->id);
-	pthread_mutex_unlock(&philo->data->status);
+	print_status(philo, philo->data->start, "has taken a fork");
+	if (philo->id == 6)
+		usleep(350);
+	print_status(philo, philo->data->start, "is eating");
 	return (0);
 }
 
@@ -35,9 +38,10 @@ void	*philo_cycle(void *ptr)
 	t_philo	*philo;
 
 	philo = (t_philo *)ptr;
-	while (1)
-	{
-		philo_eating(philo);
-	}
+	philo_eating(philo);
+	// while (1)
+	// {
+	// 	philo_eating(philo);
+	// }
 	return (0);
 }
